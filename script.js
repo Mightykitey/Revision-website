63fb5461ec098dd7700239e9a27a4e6aca5702e9
Milvus Milvus <138457184+Mightykitey@users.noreply.github.com>
2025-03-21 11:46:11 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("changeFontLink").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default link behavior (no page reload)
-
-    // Change font of the entire body or specific element (like the paragraph)
-    document.body.classList.toggle("font-changed");
-});
